1c20dd27b124b33e58098a01c1c255c7c47a34c3
MorenoRT1402 <morenort1402@gmail.com>
2024-09-16 14:35:54 +0200

    FEAT: created -> room tests

--- src/index.test.js
+++ src/index.test.js
@@ -22,49 +22,59 @@ describe('Room Class Tests', () => {
     test('should return false if the room is not occupied on a given date', () => {
       // !rooms without bookings
       const dateToCheck = new Date('2024-01-01');
-      expect(rooms[6].isOccupied(dateToCheck)).toBe(false);
-      expect(rooms[985].isOccupied(dateToCheck)).toBe(false);
+      expect(rooms[6].isOccupied(dateToCheck)).toBeFalsy();
+      expect(rooms[985].isOccupied(dateToCheck)).toBeFalsy();
     });
 
     test('should return true if the room is occupied on a given date', () => {
       // !rooms with bookings in date
       const dateToCheck = new Date('2024-09-16'); // !date in room bookings
-      expect(rooms[46].isOccupied(dateToCheck)).toBe(true);
-      expect(rooms[450].isOccupied(dateToCheck)).toBe(true); //checkIn in same date
-      expect(rooms[241].isOccupied(dateToCheck)).toBe(false); //checkOut in same date
+      expect(rooms[46].isOccupied(dateToCheck)).toBeTruthy();
+      expect(rooms[450].isOccupied(dateToCheck)).toBeTruthy(); //checkIn in same date
+      expect(rooms[241].isOccupied(dateToCheck)).toBeFalsy(); //checkOut in same date
     });
   });
 
   describe('Method: occupancyPercentage', () => {
+    const startDate = new Date('2024-01-01');
+    const endDate = new Date('2024-01-31');
+    
+    test('should throw an error if check out before check in', () => {
+        const roomInvalidChecks = rooms[19];
+        const errorMessage = 'Check In must be before Check Out';
+        expect(() => roomInvalidChecks.occupancyPercentage(startDate, endDate)).toThrow(errorMessage);
+        expect(() => rooms[0].occupancyPercentage(startDate, '2023-11-28')).toThrow(errorMessage);
+    });
+
+    test('should return 0 if no bookings', () => {
+        expect(rooms[6].occupancyPercentage(startDate, endDate)).toBe(0);
+    });
     
     test('should calculate occupancy percentage correctly', () => {
-      const room = rooms[2];
-      const startDate = new Date('2024-01-01');
-      const endDate = new Date('2024-01-31');
-      // Ajusta el valor esperado según tus datos de prueba
-      expect(room.occupancyPercentage(startDate, endDate)).toBeCloseTo(25); 
+      expect(rooms[5].occupancyPercentage(startDate, endDate)).toBeCloseTo(25); 
+      expect(rooms[15].occupancyPercentage(startDate, endDate)).toBeCloseTo(100);
+      expect(rooms[772].occupancyPercentage(startDate, endDate)).toBeCloseTo(0); //Dates no match
     });
   });
 
-  // Prueba para el método estático totalOccupancyPercentage
   describe('Static Method: totalOccupancyPercentage', () => {
 
     test('should calculate total occupancy percentage across all rooms', () => {
-      const startDate = new Date('2024-01-01');
-      const endDate = new Date('2024-01-31');
-      // Ajusta el valor esperado según tus datos de prueba
-      expect(Room.totalOccupancyPercentage(rooms, startDate, endDate)).toBeCloseTo(20); 
+      const startDate = new Date('2023-12-29');
+      const endDate = new Date('2024-07-15');
+      const roomsToCheck = rooms.slice(10, 12);
+      expect(Room.totalOccupancyPercentage(roomsToCheck, startDate, endDate)).toBeCloseTo(100);
+      expect(Room.totalOccupancyPercentage(rooms.slice(29, 31), startDate, endDate)).toBe(0);
     });
   });
 
-  // Prueba para el método estático availableRooms
   describe('Static Method: availableRooms', () => {
 
     test('should return rooms that are available for the entire date range', () => {
-      const startDate = new Date('2024-03-01');
-      const endDate = new Date('2024-03-31');
-      // Ajusta el valor esperado según tus datos de prueba
-      expect(Room.availableRooms(rooms, startDate, endDate)).toEqual([rooms[4], rooms[5]]); 
+        const startDate = new Date('2023-12-29');
+        const endDate = new Date('2024-07-15');
+        expect(Room.availableRooms(rooms.slice(29, 32), startDate, endDate)).toEqual([rooms[29], rooms[30]]);
+        expect(Room.availableRooms(rooms[35], startDate, endDate)).toEqual([]); //Not available
     });
   });
 
